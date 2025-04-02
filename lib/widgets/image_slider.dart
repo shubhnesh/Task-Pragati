01e62e82e907b69db9ff0116fc7e711ae954b8d0
Shubhnesh <kashyapshubhnesh@gmail.com>
2025-04-02 15:33:05 +0530

    Modified

--- lib/widgets/image_slider.dart
+++ lib/widgets/image_slider.dart
@@ -26,7 +26,8 @@ class _ProjectSliderState extends State<ImageSlider> {
   @override
   void initState() {
     super.initState();
-    _sliderController = PageController(initialPage: 0, viewportFraction: 0.75);
+    _sliderController = PageController();
+    // _activeSlideIndex = widget.imageList.length * 1000;
   }
 
   @override
@@ -41,124 +42,93 @@ class _ProjectSliderState extends State<ImageSlider> {
       height: widget.sliderHeight,
       child: Column(
         children: [
-          // Image slider in a stack
-          Container(
-            height: 220,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                // Image slider with curved animation
-                Positioned(
-                  height: 200,
-                  width: MediaQuery.of(context).size.width,
-                  child: PageView.builder(
-                    controller: _sliderController,
-                    onPageChanged: (int page) {
-                      setState(() {
-                        // keep track of current image index using modulo
-                        _activeSlideIndex = page % widget.imageList.length;
+          SizedBox(
+            height: 200,
+            child: PageView.builder(
+              controller: _sliderController,
+              onPageChanged: (int page) {
+                setState(() {
+                  // Update the active slide index
+                  _activeSlideIndex = page;
+                });
+              },
+              // infinite number of pages
+              itemCount:
+                  widget.imageList.length, // Limited to the number of images
+              itemBuilder: (context, index) {
+                return AnimatedBuilder(
+                  animation: _sliderController,
+                  builder: (context, child) {
+                    double value = 0;
+                    if (_sliderController.position.haveDimensions) {
+                      value = index.toDouble() - (_sliderController.page ?? 0);
+                      value = (value * 0.5).clamp(-1.0, 1.0);
+                    }
 
-                        // jump if we get too close to either end to prevent unwanted changes
-                        if (page < widget.imageList.length * 5) {
-                          // jump forward, after reaching to the begining
-                          _sliderController.jumpToPage(
-                            page + widget.imageList.length * 10,
-                          );
-                        } else if (page > widget.imageList.length * 15) {
-                          // If getting close to the end, jump backward
-                          _sliderController.jumpToPage(
-                            page - widget.imageList.length * 10,
-                          );
-                        }
-                      });
-                    },
-                    // infinite number of pages
-                    itemCount: null,
-                    itemBuilder: (context, index) {
-                      // Use modulo to repeat images for infinite scrolling
-                      final imageIndex = index % widget.imageList.length;
+                    double verticalOffset = 120 * value * value;
 
-                      return AnimatedBuilder(
-                        animation: _sliderController,
-                        builder: (context, child) {
-                          double value = 0;
-                          if (_sliderController.position.haveDimensions) {
-                            value =
-                                index.toDouble() -
-                                (_sliderController.page ?? 0);
-                            value = (value * 0.5).clamp(-1.0, 1.0);
-                          }
+                    double scaleFactor = 0.85 + (0.15 * (1 - value.abs()));
 
-                          double verticalOffset = 120 * value * value;
-
-                          double scaleFactor =
-                              0.85 + (0.15 * (1 - value.abs()));
-
-                          return Transform.translate(
-                            offset: Offset(0, verticalOffset),
-                            child: Transform.scale(
-                              scale: scaleFactor,
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: child,
+                    return Transform.translate(
+                      offset: Offset(0, verticalOffset),
+                      child: Transform.scale(
+                        scale: scaleFactor,
+                        child: Align(alignment: Alignment.center, child: child),
+                      ),
+                    );
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(milliseconds: 300),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.shadowDark.withOpacity(0.2),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset(
+                        widget.imageList[index],
+                        height: 204, // Updated size
+                        width: 275, // Updated size
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            height: 204, // Updated size
+                            width: 275, // Updated size
+                            color: AppColors.darkPurpleBackground,
+                            child: Center(
+                              child: Icon(
+                                Icons.construction,
+                                size: 48,
+                                color: AppColors.primaryPurple,
                               ),
                             ),
                           );
                         },
-                        child: AnimatedContainer(
-                          duration: Duration(milliseconds: 300),
-                          margin: EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(16),
-                            boxShadow: [
-                              BoxShadow(
-                                color: AppColors.shadowDark.withOpacity(0.2),
-                                blurRadius: 10,
-                                spreadRadius: 2,
-                              ),
-                            ],
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(16),
-                            child: Image.asset(
-                              widget.imageList[imageIndex],
-                              height: 204, // Updated size
-                              width: 275, // Updated size
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return Container(
-                                  height: 204, // Updated size
-                                  width: 275, // Updated size
-                                  color: AppColors.darkPurpleBackground,
-                                  child: Center(
-                                    child: Icon(
-                                      Icons.construction,
-                                      size: 48,
-                                      color: AppColors.primaryPurple,
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                      );
-                    },
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                );
+              },
             ),
           ),
+          const SizedBox(height: 16),
 
-          // Project name should be below the images
+          //Title of active index
           Text(
             widget.titleList[_activeSlideIndex],
             style: context.sliderItemText,
           ),
 
-          SizedBox(height: 12),
+          const SizedBox(height: 12),
 
-          // Dots indicator
+          // Dot indicator
           DotIndicator(
             count: widget.imageList.length,
             currentIndex: _activeSlideIndex,
